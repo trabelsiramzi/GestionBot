c78f7fde085bf9923740cc3020a5abae67efc508
oussama medfai <oussama.medfai@witik.io>
2022-11-30 20:08:43 +0100

    fix terminal

--- client/src/views/training/Training.js
+++ client/src/views/training/Training.js
@@ -5,18 +5,19 @@ import { CCard, CCardBody, CCardHeader, CCol, CRow, CButton } from '@coreui/reac
 import Terminal from 'terminal-in-react'
 
 const Training = () => {
+  const [results, setResult] = useState()
+  const [isLoading, setIsLoading] = useState(false)
   const token = Cookies.get('token')
   const headers = { Authorization: `Bearer ${token}` }
-  const runTraining = (cmd, print) => {
-    console.log(cmd)
-
+  const runTraining = () => {
     axios
       .get(`${process.env.REACT_APP_API}/api/training`, { headers })
       .then((response) => {
-        print(`-${response}`)
+        setResult(response.data)
+        setIsLoading(true)
       })
       .catch((e) => {
-        print(`-eroor${e}}`)
+        console.log(e)
       })
   }
 
@@ -28,35 +29,37 @@ const Training = () => {
             <strong>lancer l&apos;entrainement</strong>
           </CCardHeader>
           <CCardBody>
-            {/* <CButton color="success" size="lg" href="/json/new">
-              Nouveau Tag
-            </CButton> */}
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-              }}
-            >
-              <Terminal
-                color="white"
-                backgroundColor="black"
-                barColor="black"
-                style={{ fontWeight: 'bold', fontSize: '1em' }}
-                // commands={{
-                //   'run-training': runTraining(),
-                // }}
-                commandPassThrough={runTraining}
-                descriptions={{
-                  'open-google': 'opens google.com',
-                  showmsg: 'shows a message',
-                  alert: 'alert',
-                  popup: 'alert',
+            <CButton color="success" size="lg" onClick={runTraining}>
+              lancer l&apos;entrainement
+            </CButton>
+            {isLoading && (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100vh',
                 }}
-                msg="You can write anything here. Example - Hello! My name is Foo and I like Bar."
-              />
-            </div>
+              >
+                <Terminal
+                  color="white"
+                  backgroundColor="black"
+                  barColor="black"
+                  style={{ fontWeight: 'bold', fontSize: '1em' }}
+                  // commands={{
+                  //   'run-training': runTraining(),
+                  // }}
+                  commandPassThrough={runTraining}
+                  descriptions={{
+                    'open-google': 'opens google.com',
+                    showmsg: 'shows a message',
+                    alert: 'alert',
+                    popup: 'alert',
+                  }}
+                  msg={results}
+                />
+              </div>
+            )}
           </CCardBody>
         </CCard>
       </CCol>
